6525bd8889649443c1eb7f22b4727a3c5922cbc2
Dylan Roberts <dylangardnerroberts@gmail.com>
2021-04-21 17:03:07 -0400

    More playing with variables

--- annotations/variables.ts
+++ annotations/variables.ts
@@ -1,1 +1,26 @@
-const apples: number = 5;
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothing: null = null;
+let nada: undefined = undefined;
+
+//built in objs
+let now: Date = new Date();
+
+// arr
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1,2,3];
+let truths: boolean[] = [true,false,true];
+
+
+// classes
+class Car {
+
+}
+let car: Car = new Car();
+
+// Object literal
+let point: { x: number; y: number } = {
+    x: 10,
+    y: 20
+}
